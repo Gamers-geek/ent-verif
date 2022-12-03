3d6c7efb2dc84e5bb4b1405a69e559baf846f14c
Gamers-geek <contact@gamers-geek.gq>
2022-12-03 19:14:27 +0100

    Ajout de la fonction « devoirs »

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const dotenv = require('dotenv').config()
-const pronote = require("@dorian-eydoux/pronote-api");
+const dotenv = require("dotenv").config();
+const pronote = require("@gamers-geek/pronote-api");
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 8000;
@@ -17,12 +17,7 @@ app.post("/verifyEnt", (req, res) => {
 	const password = req.body.password;
 
 	pronote
-		.login(
-			process.env.PRONOTE_URL,
-			username,
-			password,
-			process.env.CAS
-		)
+		.login(process.env.PRONOTE_URL, username, password, process.env.CAS)
 		.then((session) => {
 			const userIdentity = session.user.name;
 			const userClass = session.user.studentClass.name;
@@ -82,11 +77,94 @@ app.post("/verifyEnt", (req, res) => {
 						"Une erreur s'est produite durant la connexion à votre compte ENT. Veuillez réessayer.",
 				};
 				return res.status(501).json(data);
+			} else {
+				return res.status(501).json({ error: true, message: `${err}` });
+			}
+		});
+});
+
+app.post("/homeworks", (req, res) => {
+	const username = req.body.username;
+	const password = req.body.password;
+
+	if (!username || !password) {
+		data = {
+			error: true,
+			message: "Champs manquants.",
+		};
+		return res.status(400).json(data);
+	}
+
+	pronote
+		.login(process.env.PRONOTE_URL, username, password, process.env.CAS)
+		.then(async (session) => {
+			const userIdentity = session.user.name;
+			const userClass = session.user.studentClass.name;
+			const homeworks = await session.homeworks();
+			session.logout();
+			data = {
+				user: userIdentity,
+				class: userClass,
+				homeworks: homeworks,
+			};
+			res.status(200).json(data);
+		})
+		.catch((err) => {
+			if (err.code == pronote.errors.WRONG_CREDENTIALS.code) {
+				data = {
+					error: true,
+					message: "Identifiants ENT invalides, veuillez réessayer.",
+				};
+				return res.status(501).json(data);
+			} else if (err.code == pronote.errors.BANNED.code) {
+				data = {
+					error: true,
+					message:
+						"Nous n'avons pas réussi à vérifier votre compte, car votre IP est provisoirement bannie de pronote. Veuillez réessayer dans 30 minutes.",
+				};
+				return res.status(501).json(data);
+			} else if (err.code == pronote.errors.CLOSED.code) {
+				data = {
+					error: true,
+					message:
+						"Le Pronote de votre établissement scolaire est provisoirement fermé. Veuillez réessayer lorsque ce dernier sera rouvert.",
+				};
+				return res.status(501).json(data);
+			} else if (err.code == pronote.errors.PRONOTE.code) {
+				data = {
+					error: true,
+					message:
+						"Une erreur générique s'est produite du côté du serveur de Pronote. Veuillez réessayer.",
+				};
+				return res.status(501).json(data);
+			} else if (err.code == pronote.errors.RATE_LIMITED.code) {
+				data = {
+					error: true,
+					message:
+						"Nous n'avons pas réussi à vérifier votre compte, car votre IP est provisoirement bannie de pronote. Veuillez réessayer dans 30 minutes.",
+				};
+				return res.status(501).json(data);
+			} else if (err.code == pronote.errors.UNKNOWN_ACCOUNT.code) {
+				data = {
+					error: true,
+					message:
+						"Une erreur s'est produite durant la vérification de votre compte. Veuillez réessayer.",
+				};
+				return res.status(501).json(data);
+			} else if (err.code == pronote.errors.UNKNOWN_CAS.code) {
+				data = {
+					error: true,
+					message:
+						"Une erreur s'est produite durant la connexion à votre compte ENT. Veuillez réessayer.",
+				};
+				return res.status(501).json(data);
+			} else {
+				return res.status(501).json({ error: true, message: `${err}` });
 			}
 		});
 });
 
 app.post("/debug", (req, res) => {
-	if(process.env.DEBUG != "ENABLED") return res.status(404)
-	return res.status(200).json(req.body) 
-})
+	if (process.env.DEBUG != "ENABLED") return res.status(404);
+	return res.status(200).json(req.body);
+});
